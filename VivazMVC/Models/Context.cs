4da8539d165ef795944a30483f5b5094245d4e78
Kendysan88 <kendysan@gmail.com>
2021-04-07 14:40:17 -0300

    Subindo VivazMVC e VivazAPI

---
+++ VivazMVC/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VivazMVC.Models
+{
+    public class Context : DbContext
+    {
+        //Método que configura o  Entity Framework
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString: @"User ID=XXXXXXX;Password=XXXXXXX;Host=localhost;Port=5432;Database=NomeDoBanco;Pooling=true;");
+        }
+    }
+}
