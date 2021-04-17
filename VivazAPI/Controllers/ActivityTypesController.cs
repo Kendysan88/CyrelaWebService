932f5791f2aee7a983be638f5a5819182754ba27
AfonSir <afonsir17@gmail.com>
2021-04-17 16:02:03 -0300

    refactor: implement automapper aproach in activity types

--- VivazAPI/Controllers/ActivityTypesController.cs
+++ VivazAPI/Controllers/ActivityTypesController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using VivazAPI.Data;
+using VivazAPI.Dtos;
 using VivazAPI.Models;
 
 namespace VivazAPI.Controllers
@@ -10,16 +13,19 @@ namespace VivazAPI.Controllers
     {
         private readonly IRepository<ActivityType> _repository;
 
-        public ActivityTypesController(IRepository<ActivityType> repository)
+        private readonly IMapper _mapper;
+
+        public ActivityTypesController(IRepository<ActivityType> repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
         
         [HttpGet]
         public IActionResult Get()
         {
             var activityTypes = _repository.FindAll();
-            return Ok(activityTypes);
+            return Ok(_mapper.Map<IEnumerable<ActivityTypeReadDto>>(activityTypes));
         }
     }
 }
