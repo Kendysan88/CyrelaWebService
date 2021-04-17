ed54e2dd15f9b2a43906b74e94825fd62e596fc3
AfonSir <afonsir17@gmail.com>
2021-04-17 18:22:15 -0300

    feat: add migration to create occurences

--- VivazAPI/Data/DataContext.cs
+++ VivazAPI/Data/DataContext.cs
@@ -14,5 +14,6 @@ namespace VivazAPI.Data
         public DbSet<ActivityType> ActivityTypes { get; set; }
         public DbSet<AddressState> AddressStates { get; set; }
         public DbSet<Building> Buildings { get; set; }
+        public DbSet<Occurence> Occurences { get; set; }
     }
 }
---
+++ VivazAPI/Migrations/20210417212107_CreateOccurences.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VivazAPI.Migrations
+{
+    public partial class CreateOccurences : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Occurences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BuildingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActivityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActivityTypeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occurences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Occurences_ActivityTypes_ActivityTypeId",
+                        column: x => x.ActivityTypeId,
+                        principalTable: "ActivityTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Occurences_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Occurences_Users_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Occurences_ActivityTypeId",
+                table: "Occurences",
+                column: "ActivityTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Occurences_BuildingId",
+                table: "Occurences",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Occurences_CustomerId",
+                table: "Occurences",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Occurences");
+        }
+    }
+}
