3b5d76adba6bb365b47c9f5de4b7a4690a747fc4
Kendysan88 <kendysan@gmail.com>
2021-04-19 22:10:05 -0300

    Removing Constructor from DataContext

--- VivazAPI/Data/DataContext.cs
+++ VivazAPI/Data/DataContext.cs
@@ -7,11 +7,7 @@ using VivazAPI.Models;
 namespace VivazAPI.Data
 {
     public class DataContext : DbContext
-    {
-        public DataContext()
-        {
-        }
-
+    {   
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<User> Users { get; set; }
